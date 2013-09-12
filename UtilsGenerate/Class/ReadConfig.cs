10c7ab2b0dff3fe777664d7bae5132afc782ec32
sercheo87 <chancaysergio@gmail.com>
2013-09-12 09:03:31 -0700

    ok

---
+++ UtilsGenerate/Class/ReadConfig.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Reflection;
+using System.IO;
+
+namespace UtilsGenerate
+{
+    public class ReadConfig
+    {
+        private XDocument document = XDocument.Load(Path.Combine( Directory.GetParent(Assembly.GetExecutingAssembly().Location.ToString()).ToString(),"ProjectConfig.xml"));
+        public string GetSolutionLocation()
+        {
+            XElement xElement = this.document.Descendants("config").Elements("SolutionLocation").SingleOrDefault<XElement>();
+            return xElement.Value;
+        }
+        public string GetDirectoryOut()
+        {
+            XElement xElement = this.document.Descendants("config").Elements("DirectoryOut").SingleOrDefault<XElement>();
+            return xElement.Value;
+        }
+        public string GetClickOnceLocation()
+        {
+            XElement xElement = this.document.Descendants("config").Elements("ClickOnceLocation").SingleOrDefault<XElement>();
+            return xElement.Value;
+        }
+        public string GetClickOncePefix()
+        {
+            XAttribute xAttribute = this.document.Descendants("config").Elements("ClickOnceLocation").Attributes("prefix").SingleOrDefault<XAttribute>();
+            return xAttribute.Value;
+        }
+        public string GetRelations(string arg1)
+        {
+            XElement xElement = (
+                from x in this.document.Descendants("config").Elements("relations").Elements("relation")
+                where x.Element("project").Value == arg1
+                select x).Elements("installer").SingleOrDefault<XElement>();
+            string result;
+            if (xElement != null)
+            {
+                result = xElement.Value;
+            }
+            else
+            {
+                result = null;
+            }
+            return result;
+        }
+    }
+}
---
+++ UtilsGenerate/Class/UtilsProjects.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace UtilsGenerate
+{
+    public class UtilsProjects
+    {
+        public DtoProject[] FindProjects(string path)
+        {
+
+            ReadConfig XmlDoc = new ReadConfig();
+            XmlDoc.GetClickOncePefix();
+            List<DtoProject> ret = new List<DtoProject>();
+            int id = 1;
+            foreach (string item in Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).Where(file=>file.EndsWith("csproj")| file.EndsWith("vbproj")))
+            {
+                ret.Add(new DtoProject()
+                    {
+                        id = id++,
+                        FullPath = item,
+                        Name = Path.GetFileNameWithoutExtension(item),
+                        ClickOnceSolution = XmlDoc.GetRelations(Path.GetFileNameWithoutExtension(item))
+                    });
+            }
+            return ret.ToArray();
+        }
+    }
+
+    public class DtoProject
+    {
+        public int id { get; set; }
+        public string FullPath { get; set; }
+        public string Name { get; set; }
+        public string ClickOnceSolution { get; set; }
+    }
+}
